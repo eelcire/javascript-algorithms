5481df7d3787fa331dd0ad443d5a03ebf883256c
eelcire <eelcire.eelcire@hotmail.com>
2019-12-17 00:07:51 -0600

    reverse string done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let answer = '';
+    for (let i = 0; i < str.length; i++) {
+        answer = str[i] + answer;
+        debugger;
+    }
+    return answer
+}
+
+// function reverse(str) {
+//     let answer = ''
+//     for (let thing of str) {
+//         answer = thing + answer
+//     }
+//     return answer
+// }
+
+// function reverse(str) {
+//     return str.split("").reverse().join("")
+// }
+
+// function reverse(str) {
+//     return str.split('').reduce((reversed, char) => char + reversed, '')
+// }
 
 module.exports = reverse;
