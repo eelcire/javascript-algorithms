e1c7a355058e3a52ff765651f70f66c908de381c
eelcire <eelcire.eelcire@hotmail.com>
2019-12-19 00:38:15 -0500

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let max = 0
+    let maxChar = ''
+    let set = new Set();
+    for (let char of str) {
+        if (set.has(char)) {
+            set[char]++
+        } else {
+            set[char] = 1
+        }
+    }
+    for (let char in set) {
+        if (set[char] > max) {
+            max = set[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
