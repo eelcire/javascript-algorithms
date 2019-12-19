66e957cadadfd54b95482fc1deffe0a323072592
eelcire <eelcire.eelcire@hotmail.com>
2019-12-18 23:44:39 -0500

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let stringed = n.toString();
+    let answer = ''
+    for (let char of stringed) {
+        answer = char + answer
+    }
+    if (stringed[0] === '-') {
+        answer = '-' + answer.slice(0, -1)
+    }
+    return parseInt(answer)
+}
+
+// function reverseInt(n) {
+//     let sign = Math.sign(n)
+//     let answer = n.toString().split('').reverse().join('')
+//     return parseInt(answer) * sign
+// }
+
+
 
 module.exports = reverseInt;
