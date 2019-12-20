8f977ae4441d4c079bce385d1513c886246c05cf
eelcire <eelcire.eelcire@hotmail.com>
2019-12-20 02:20:10 -0500

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,49 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//     let answer = []
+//     let counter = 0
+//     let counter2 = 0
+//     while (counter < array.length)
+//         if (!answer[counter2]) {
+//             answer[counter2] = [array[counter]]
+//             counter++
+//         } else if (answer[counter2] && answer[counter2].length < size) {
+//             answer[counter2].push(array[counter])
+//             counter++
+//         } else {
+//             counter2++
+//             answer[counter2] = [array[counter]]
+//             counter++
+//         }
+//     return answer
+// }
+
+// function chunk(array, size) {
+//     let answer = []
+
+//     for (let item of array) {
+//         let last = answer[answer.length - 1]
+
+//         if (!last || last.length === size) {
+//             answer.push([item])
+//         } else {
+//             last.push(item)
+//         }
+//     }
+//     return answer
+// }
+
+function chunk(array, size) {
+    let answer = []
+    let pointer = 0;
+
+    while (pointer < array.length) {
+        answer.push(array.slice(pointer, pointer + size))
+        pointer += size
+    }
+    return answer
+}
 
 module.exports = chunk;
