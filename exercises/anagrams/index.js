3c02f8aa3a0aa9b353638aff679ffef1eb5e93d8
eelcire <eelcire.eelcire@hotmail.com>
2019-12-20 22:17:31 -0500

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//     let newA = stringA.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+//     let newB = stringB.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+    
+//     return newA === newB
+// }
+
+function anagrams(stringA, stringB) {
+    let newA = stringA.replace(/[^w]/g, '').toLowerCase()
+    let newB = stringB.replace(/[^w]/g, '').toLowerCase()
+    let mapA = {}
+    let mapB = {}
+
+    if (newA.length !== newB.length) return false
+
+    for (let char of stringA) {
+        mapA[char] = mapA[char] + 1 || 1
+    }
+    for (let char of stringB) {
+        mapB[char] = mapB[char] + 1 || 1
+    }
+    for (let char in mapA) {
+        if (mapA[char] !== mapB[char]) return false
+    }
+    return true
+}   
 
 module.exports = anagrams;
