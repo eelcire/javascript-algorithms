3bf161e203046f513222b6d0a2cef2129c4260e2
eelcire <eelcire.eelcire@hotmail.com>
2019-12-22 19:26:10 -0500

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,33 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//     let list = [0, 1]
+//     for (let i = 2; i <= n; i++) {
+//         list[i] = list[i - 1] + list[i - 2]
+//     }
+//     return list[n]
+// }
+
+function memoize(fn) {
+    let cache = {}
+    return function(arg) {
+        if (cache[arg]) {
+            return cache[arg]
+        }
+        let result = fn(arg)
+        cache[arg] = result
+        return result
+    }
+}
+
+function slowFib(n) {
+    if (n < 2) {
+        return n
+    }
+    return fib(n - 1) + fib(n - 2)
+}
+
+const fib = memoize(slowFib)
 
 module.exports = fib;
