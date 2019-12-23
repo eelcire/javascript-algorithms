86da1aadfffc29010e2b9caeb84a351267eda743
eelcire <eelcire.eelcire@hotmail.com>
2019-12-22 19:56:50 -0500

    weave

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,17 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+    const a = new Queue()
+    while (sourceOne.peek() || sourceTwo.peek()) {
+        if (sourceOne.peek()) {
+            a.add(sourceOne.remove())
+        }
+        if (sourceTwo.peek()) {
+            a.add(sourceTwo.remove())
+        }
+    }
+    return a
+}
 
 module.exports = weave;
